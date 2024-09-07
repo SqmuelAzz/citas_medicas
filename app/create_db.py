4a0bce2b72fdebdeee5549cce4e3b82d5b3ea14c
Natanael Herrera <nhzamorano@hotmail.com>
2024-09-06 20:08:40 -0500

    Script pra crear DB y main.py

---
+++ app/create_db.py
@@ -0,0 +1,140 @@
+import sqlite3
+
+class Create_db:
+    def __init__(self):
+        self.conexion = sqlite3.connect('citas_medicas.sqlite3')
+        self.cursor = self.conexion.cursor()
+    
+    
+    def Crear_Tablas(self):
+        resp = input("Desea crear la estructura de la base de datos, se elliminaran todos los datos existentes, <S/N>: ")
+        if resp.upper() == 'S':
+            print("Creando estructura de la base de datos BIBLIOTECA")
+            print()
+            print("Creando tabala usuarios ...")
+            self.cursor.execute("""
+                DROP TABLE IF EXISTS pacientes;
+            """)
+
+            self.cursor.execute("""
+                CREATE TABLE IF NOT EXISTS pacientes(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL,
+                email TEXT NOT NULL,
+                telefono TEXT NOT NULL,
+                direccion TEXT,
+                preferencia_notificacion TEXT NOT NULL,
+                fecha_creacion date DEFAULT (DATE('now'))
+                );
+            """)
+
+            print()
+            print("Creando tabala medicos ...")
+            self.cursor.execute("""
+                DROP TABLE IF EXISTS medicos;
+            """)
+
+            self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS medicos(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL,
+                especialidad TEXT NOT NULL,
+                id_agenda INTEGER NOT NULL,
+                id_disponibilidad INTEGER NOT NULL
+                );
+            """)
+            print()
+            print("Creando tabala citas ...")
+            self.cursor.execute("""
+                DROP TABLE IF EXISTS citas;
+            """)
+
+            self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS citas(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                fecha  date DEFAULT (DATE('now')),
+                id_paciente INTEGER NOT NULL,
+                id_medico INTEGER NOT NULL,
+                detalle TEXT,
+                estado TEXT NOT NULL,
+                duracion INTEGER NOT NULL,
+                fecha_creacion date DEFAULT (DATE('now')),
+                FOREIGN KEY (id_paciente) REFERENCES pacientes(id),
+                FOREIGN KEY (id_medico) REFERENCES medicos(id)
+                );
+            """)
+
+            print()
+            print("Creando tabala horario ...")
+            self.cursor.execute("""
+                DROP TABLE IF EXISTS horario;
+            """)
+            
+            #hora_inicio: '14:30:00'
+            self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS horario(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                dia TEXT NOT NULL,
+                hora_inicio TEXT NOT NULL, 
+                hora_fin TEXT NOT NULL,
+                id_medico INTEGER NOT NULL,
+                FOREIGN KEY (id_medico) REFERENCES medicos(id)
+                );
+            """)
+
+            print()
+            print("Creando tabala administrador ...")
+            self.cursor.execute("""
+                DROP TABLE IF EXISTS administrador;
+            """)
+            #loan_type: domiciliario, sala
+            self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS administrador(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL,
+                book_id INTEGER NOT NULL,
+                fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                );
+            """)
+
+            print()
+            print("Creando tabala notificaciones ...")
+            self.cursor.execute("""
+                DROP TABLE IF EXISTS notificaciones;
+            """)
+
+            #state pending,paid
+            self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS notificaciones(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                id_paciente INTEGER NOT NULL,
+                id_cita INTEGER NOT NULL,
+                mensaje TEXT NOT NULL,
+                medio TEXT NOT NULL,
+                fecha_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                estado TEXT NOT NULL,    
+                FOREIGN KEY(id_paciente) REFERENCES pacientes(id),
+                FOREIGN KEY(id_cita) REFERENCES citas(id)
+                );
+            """)
+
+            print()
+            print("Creando tabala reportes ...")
+            self.cursor.execute("""
+                DROP TABLE IF EXISTS reservations;
+            """)
+
+            #state: active, completed, cancelled
+            self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS reportes(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                id_adm INTEGER NOT NULL,
+                tipo_reporte TEXT NOT NULL,
+                contenido TEXT NOT NULL,
+                fecha_generacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY(id_adm) REFERENCES administrador(id)
+                );
+            """)
+    
+db=Create_db()
+db.Crear_Tablas()
